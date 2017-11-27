c2ec076d9624ef1c92a38b670e58f1ed99391a98
kazartseva <kazarceva@gmail.com>
2017-11-27 15:07:51 +0100

    Добавлет пакет

--- sandbox/src/main/java/it/java/sandbox/myFirstProgramm.java
+++ sandbox/src/main/java/it/java/sandbox/myFirstProgramm.java
@@ -1,4 +1,6 @@
-public class task1 {
+package it.java.sandbox;
+
+public class myFirstProgramm {
 	public static void main(String[] args) {
 		System.out.println("Задание 1: Подготовить инфраструктуру и создать первую работающую программу");
 	}
