b3234e22adbe1a26a95e86bc0211346ae19900bc
kazartseva <kazarceva@gmail.com>
2017-11-30 21:22:26 +0100

    Добавлено несколько функций

--- sandbox/src/main/java/it/java/sandbox/myFirstProgramm.java
+++ sandbox/src/main/java/it/java/sandbox/myFirstProgramm.java
@@ -1,8 +1,32 @@
 package it.java.sandbox;
 
 public class myFirstProgramm {
-	public static void main(String[] args) {
-		System.out.println("Задание 1: Подготовить инфраструктуру и создать первую работающую программу");
-	}
-		
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Maria");
+
+    double l = 5;
+    System.out.println("Площадь квардрата со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника с сторонами " + a + " и " + b + " = " + area(a, b));
+  }
+
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
 }
+
