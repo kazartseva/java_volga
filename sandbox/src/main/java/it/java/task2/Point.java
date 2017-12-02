940c10f7f480cd58be3d9dee5447dca39ec73ea1
kazartseva <kazarceva@gmail.com>
2017-12-02 19:14:26 +0100

    Метод distance добавлен в класс Point, добавлены примеры вызова метода

--- sandbox/src/main/java/it/java/task2/Point.java
+++ sandbox/src/main/java/it/java/task2/Point.java
@@ -4,10 +4,13 @@ public class Point {
   public double x;
   public double y;
 
-  public Point (double x, double y) {
+  public Point(double x, double y) {
     this.x = x;
     this.y = y;
   }
 
+  public double distance(Point p) {
+    return Math.sqrt(Math.pow(this.x - p.x,2) + Math.pow(this.y - p.y,2));
+  }
 
 }
--- sandbox/src/main/java/it/java/task2/task2.java
+++ sandbox/src/main/java/it/java/task2/task2.java
@@ -6,10 +6,14 @@ public class task2 {
     Point p2 = new Point(10,4);
 
     System.out.println("Расстояние между точками с координатами (" + p1.x + "," + p2.y + ") и (" + p2.x + "," + p2.y + ") = " + distance(p1, p2));
+
+    System.out.println("Расстояние между точками с координатами (" + p1.x + "," + p2.y + ") и (" + p2.x + "," + p2.y + ") = " + p1.distance(p2));
+
+    System.out.println("Расстояние между точками с координатами (" + p1.x + "," + p2.y + ") и (" + p2.x + "," + p2.y + ") = " + p2.distance(p1));
   }
 
 
   public static double distance(Point p1, Point p2) {
-  return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+  return Math.sqrt(Math.pow((p2.x - p1.x),2) + Math.pow((p2.y - p1.y),2));
   }
 }
