bb9b727f60d26cf8f0d63f12b1d14c83748cf588
kazartseva <kazarceva@gmail.com>
2017-12-03 14:56:31 +0100

    Исправлено название метода теста, логин перенесен в инициализацию
    Исправлено название метода теста: в сгенерированном тесте название метода было
    1. одинаковым с названием класса (обычно конструктор имеет название как у класса, поэтому возможны два метода с одинаковым названием)
    2. начиналось с заглавной буквы. Только названия классов могут начинаться с заглавной буквы.
    
    Код логина был перенесен в инициализацию (в @before test)

--- addressbook-web-tests/src/test/java/it/java/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/it/java/addressbook/GroupCreationTests.java
@@ -22,16 +22,17 @@ public class GroupCreationTests {
     public void setUp() throws Exception {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("/Applications/Firefox.app/Contents/MacOS/firefox-bin"));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreationTests() {
         wd.get("http://localhost/addressbook/index.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).sendKeys("admin");
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+    
+    @Test
+    public void testGroupCreation() {
+
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
