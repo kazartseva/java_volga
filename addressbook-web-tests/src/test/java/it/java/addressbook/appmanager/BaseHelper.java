4a2e8300a2dfbb96761d9ffdbe6a7a10b795c1f5
kazartseva <kazarceva@gmail.com>
2017-12-10 10:56:25 +0100

    Задание #5: Добавлен класс BaseHelper в пакет пакет it.java.addressbook.appmanager
    пакет it.java.addressbook.appmanager

---
+++ addressbook-web-tests/src/test/java/it/java/addressbook/appmanager/BaseHelper.java
@@ -0,0 +1,22 @@
+package it.java.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseHelper {
+  protected FirefoxDriver wd;
+
+  public BaseHelper(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+
+  protected void type(By locator, String text) {
+    click(locator);
+    wd.findElement(locator).clear();
+    wd.findElement(locator).sendKeys(text);
+  }
+
+  protected void click(By locator) {
+    wd.findElement(locator).click();
+  }
+}
--- addressbook-web-tests/src/test/java/it/java/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/it/java/addressbook/appmanager/ContactHelper.java
@@ -4,44 +4,31 @@ import it.java.addressbook.models.ContactData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class ContactHelper {
-  private FirefoxDriver wd;
+public class ContactHelper extends BaseHelper{
 
   public ContactHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void returnToHomePage() {
-    wd.findElement(By.linkText("home page")).click();
+    click(By.linkText("home page"));
   }
 
   public void submitContactCreation() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    click(By.xpath("//div[@id='content']/form/input[21]"));
   }
 
   public void fillContactForm(ContactData contactData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(contactData.getHomenumber());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-    wd.findElement(By.name("address2")).click();
-    wd.findElement(By.name("homepage")).click();
-    wd.findElement(By.name("homepage")).clear();
-    wd.findElement(By.name("homepage")).sendKeys(contactData.getHomepage());
+    type(By.name("firstname"), contactData.getFirstname());
+    type(By.name("lastname"), contactData.getLastname());
+    type(By.name("company"), contactData.getCompany());
+    type(By.name("home"), contactData.getHomenumber());
+    type(By.name("email"), contactData.getEmail());
+    type(By.name("homepage"), contactData.getHomepage());
   }
 
   public void initContactCreation() {
-    wd.findElement(By.linkText("add new")).click();
+    click(By.linkText("add new"));
   }
+
 }
