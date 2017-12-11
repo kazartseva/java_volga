181d87b5a0647e4dfd5b85a6cb6d80d200293ba7
kazartseva <kazarceva@gmail.com>
2017-12-11 11:47:33 +0100

    Лекция 3.2. Полная форма if-then-else

--- sandbox/src/main/java/it/java/sandbox/Equation.java
+++ sandbox/src/main/java/it/java/sandbox/Equation.java
@@ -16,17 +16,32 @@ public class Equation {
 
     double d = b*b - 4*a*c;
 
-    if (d>0) {
-      n = 2;
-    }
-    else {
-      if (d==0) {
-        n = 1;
+
+
+    if (a==0) {
+      if (b == 0) {
+        if (c == 0) {
+          n = -1;
+        }
+        else {
+          n = 0;
+        }
       }
       else {
+        n = 1;
+      }
+
+    } else {
+      if (d>0) {
+        n = 2;
+      } else if (d==0) {
+        n = 1;
+      } else {
         n = 0;
       }
+
     }
+
   }
 
   public int rootNumber () {
