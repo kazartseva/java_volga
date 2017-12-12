1223c8cadb5c6fa3af61e55ea2a6bcf2b89a247b
kazartseva <kazarceva@gmail.com>
2017-12-12 22:03:38 +0100

    Лекция 3.3. Запуск тестов в разных браузерах
    добавлены новые переменные: wd - веб драйверы различных браузеров

--- addressbook-web-tests2/src/test/java/it/java/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests2/src/test/java/it/java/addressbook/appmanager/ApplicationManager.java
@@ -1,28 +1,44 @@
 package it.java.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-  FirefoxDriver wd;
+  WebDriver wd;
 
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private String browser;
 
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
 
   public void init() {
-    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+      if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    } else if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.SAFARI) {
+      wd = new SafariDriver(new SafariOptions().setUseTechnologyPreview(true));
+    }
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/index.php");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
     sessionHelper.login("admin", "secret");
+
   }
 
 
--- addressbook-web-tests2/src/test/java/it/java/addressbook/tests/TestBase.java
+++ addressbook-web-tests2/src/test/java/it/java/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package it.java.addressbook.tests;
 
 import it.java.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.SAFARI);
 
   @BeforeMethod
   public void setUp() throws Exception {
