0b9d589afbe00a62a8cd071a47508d0ffdebf4c8
kazartseva <kazarceva@gmail.com>
2018-01-21 13:19:27 +0100

    Задание №13: Параметризовать тесты для создания групп и контактов

--- addressbook-web-tests/src/test/java/it/java/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/it/java/addressbook/tests/ContactCreationTests.java
@@ -1,42 +1,48 @@
 package it.java.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import it.java.addressbook.models.ContactData;
 import it.java.addressbook.models.Contacts;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test(enabled = true)
-  public void contactCreationTests() {
+  @DataProvider
+  public Iterator<Object[]> contacts() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+    return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "contacts")
+  public void contactCreationTests(ContactData contact) {
     app.goTo().homePage();
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/avatar.png");
-    ContactData contact = new ContactData()
-            .withFirstname("Alessandro")
-            .withLastname("Mariani")
-            .withCompany("Cinema")
-            .withHomeNumber("777")
-            .withMobileNumber("888")
-            .withWorkNumber("999")
-            .withEmail1("email1@test.com")
-            .withEmail2("email2@test.com")
-            .withEmail3("email3@test.com")
-            .withHomepage("www.cinema.it")
-            .withGroup("[none]")
-            .withAddress("address1")
-            .withAddress2("address2")
-            .withPhoto(photo);
     app.contact().createAContact(contact, true);
     assertThat(app.contact().getContactCount(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
-
 
 }
