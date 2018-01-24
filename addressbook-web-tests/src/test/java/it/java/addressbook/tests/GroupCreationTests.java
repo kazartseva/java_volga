3c5d173108fdd9d7fba31d539a719341131f58a3
kazartseva <kazarceva@gmail.com>
2018-01-24 12:53:26 +0100

    Лекция 7.2. Object Relational Mapping (ORM)

--- addressbook-web-tests/src/test/java/it/java/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/it/java/addressbook/tests/GroupCreationTests.java
@@ -8,8 +8,10 @@ import it.java.addressbook.models.Groups;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
