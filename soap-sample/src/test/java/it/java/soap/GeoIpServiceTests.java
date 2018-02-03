e5311c808428555e62548dbc3b09fff97f590726
kazartseva <kazarceva@gmail.com>
2018-02-03 19:17:03 +0100

    Лекция 9.2. SOAP: простой пример (определение геопозиции по адресу IP)
    определение геопозиции по адресу IP

---
+++ soap-sample/src/test/java/it/java/soap/GeoIpServiceTests.java
@@ -0,0 +1,16 @@
+package it.java.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIp() {
+    GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("95.235.200.241");
+    assertEquals(geoIP.getCountryCode(), "ITA");
+  }
+}
