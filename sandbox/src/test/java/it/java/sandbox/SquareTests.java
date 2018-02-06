26790249627e527bf42d81761cddbc10360897c2
kazartseva <kazarceva@gmail.com>
2018-02-06 19:08:49 +0100

    test-reports

--- sandbox/src/test/java/it/java/sandbox/SquareTests.java
+++ sandbox/src/test/java/it/java/sandbox/SquareTests.java
@@ -6,8 +6,8 @@ import org.testng.annotations.Test;
 public class SquareTests {
 @Test
   public void testArea () {
-    Square s = new Square (6);
-  Assert.assertEquals(s.area(),36.0);
+    Square s = new Square (7);
+  Assert.assertEquals(s.area(),49.0);
   }
 
 }
